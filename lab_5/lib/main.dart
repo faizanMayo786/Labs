883a834ffa136ca0af020b1a33582a4829fefa9f
faizanMayo786 <faizangame998@gmail.com>
2022-04-02 22:30:58 +0500

    Lab - 5

---
+++ lab_5/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          body: SizedBox(
+        height: double.infinity,
+        width: double.infinity,
+        child: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  expandedButton(Colors.yellow, 1),
+                  expandedButton(Colors.black, 2),
+                  expandedButton(Colors.purple, 3),
+                  expandedButton(Colors.cyan, 4),
+                  expandedButton(Colors.orange, 5),
+                  expandedButton(Colors.red, 6),
+                  expandedButton(Colors.lightGreen, 7),
+                ],
+              ),
+            ),
+          ],
+        ),
+      )),
+    );
+  }
+
+  Expanded expandedButton(Color color, int i) {
+    return Expanded(
+      child: Container(
+        height: double.infinity,
+        color: color,
+        child: TextButton(
+          onPressed: () async {
+            AudioCache audioPlayer = AudioCache();
+            await audioPlayer.play('note$i.wav');
+          },
+          child: const Text(""),
+        ),
+      ),
+    );
+  }
+}
---
+++ lab_5/lib/question1.dart
@@ -0,0 +1,43 @@
+import 'dart:math';
+
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: const BallPage(),
+  ));
+}
+
+class BallPage extends StatefulWidget {
+  const BallPage({Key? key}) : super(key: key);
+
+  @override
+  State<BallPage> createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int num = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Ask Me Anything"),
+        backgroundColor: const Color.fromARGB(255, 49, 121, 228),
+      ),
+      backgroundColor: Colors.lightBlue,
+      body: Center(
+        child: FlatButton(
+          child: Image.asset("assets/images/ball$num.png"),
+          onPressed: () {
+            setState(() {
+              num = Random().nextInt(4) + 1;
+            });
+            print("I got Clicked.");
+          },
+        ),
+      ),
+    );
+  }
+}
